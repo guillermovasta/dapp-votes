3be5f493a1210eea50fba92e4890a9e45c021030
Guillermo Vasta <guillermovasta@hotmail.com>
2020-12-02 19:27:14 -0300

    Testing out connection via MetaMask

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,41 @@
-import React from "react";
+import React, { useEffect } from 'react'
+import { Web3ReactProvider } from '@web3-react/core'
+import { Web3Provider } from '@ethersproject/providers'
+import { useWeb3React } from '@web3-react/core'
+import { InjectedConnector } from '@web3-react/injected-connector'
 
-function App() {
-  return <></>;
+const injectedConnector = new InjectedConnector({
+  supportedChainIds: [
+    4, // Rinkeby
+  ],
+})
+
+const getLibrary = (provider: any): Web3Provider => {
+  const library = new Web3Provider(provider)
+  library.pollingInterval = 12000
+  return library
 }
 
-export default App;
+export const Dashboard = () => {
+  const { chainId, account, activate, active } = useWeb3React<Web3Provider>()
+
+  useEffect(() => {
+    activate(injectedConnector)
+  }, [activate])
+
+  return (
+    <div>
+      <div>ChainId: {chainId}</div>
+      <div>Account: {account}</div>
+      <p>active: {active ? 'true' : 'false'}</p>
+    </div>
+  )
+}
+
+const App = () => (
+  <Web3ReactProvider getLibrary={getLibrary}>
+    <Dashboard />
+  </Web3ReactProvider>
+)
+
+export default App
