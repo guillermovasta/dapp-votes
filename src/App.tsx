8a7086132d69ac86a9dce5eb2a12af6c06f04747
Guillermo Vasta <guillermovasta@hotmail.com>
2020-12-04 18:20:43 -0300

    Switch to ethers library

--- src/App.tsx
+++ src/App.tsx
@@ -1,41 +1,23 @@
-import React, { useEffect } from 'react'
-import { Web3ReactProvider } from '@web3-react/core'
-import { Web3Provider } from '@ethersproject/providers'
-import { useWeb3React } from '@web3-react/core'
-import { InjectedConnector } from '@web3-react/injected-connector'
+import React from 'react'
+import {
+  createDefaultContractInstance,
+  createWeb3ContractInstance
+} from './utils'
 
-const injectedConnector = new InjectedConnector({
-  supportedChainIds: [
-    4, // Rinkeby
-  ],
-})
+const App = () => {
+  const ethereum = (window as any).ethereum
+  const web3Contract = createWeb3ContractInstance(ethereum)
+  const defaultContract = createDefaultContractInstance()
 
-const getLibrary = (provider: any): Web3Provider => {
-  const library = new Web3Provider(provider)
-  library.pollingInterval = 12000
-  return library
-}
-
-export const Dashboard = () => {
-  const { chainId, account, activate, active } = useWeb3React<Web3Provider>()
-
-  useEffect(() => {
-    activate(injectedConnector)
-  }, [activate])
+  const handleConnectToMetaMask = () => {
+    ethereum.request({ method: 'eth_requestAccounts' })
+  }
 
   return (
-    <div>
-      <div>ChainId: {chainId}</div>
-      <div>Account: {account}</div>
-      <p>active: {active ? 'true' : 'false'}</p>
-    </div>
+    <>
+      <button onClick={handleConnectToMetaMask}>Connect to MetaMask</button>
+    </>
   )
 }
-
-const App = () => (
-  <Web3ReactProvider getLibrary={getLibrary}>
-    <Dashboard />
-  </Web3ReactProvider>
-)
 
 export default App
---
+++ src/utils/index.ts
@@ -0,0 +1,29 @@
+import { ethers } from 'ethers'
+import { walletPrivateKey, contractAddress } from '../config'
+import contractInterface from '../abi/ERC20.abi.json'
+
+enum Networks {
+  rinkeby = 4
+}
+
+export const createWeb3ContractInstance = (ethereum: any) => {
+  const provider = new ethers.providers.Web3Provider(ethereum, Networks.rinkeby)
+  const signer = provider.getSigner()
+  const contract = new ethers.Contract(
+    contractAddress,
+    contractInterface,
+    signer
+  )
+  return contract
+}
+
+export const createDefaultContractInstance = () => {
+  const provider = ethers.getDefaultProvider(Networks.rinkeby.toString())
+  const wallet = new ethers.Wallet(walletPrivateKey, provider)
+  const contract = new ethers.Contract(
+    contractAddress,
+    contractInterface,
+    wallet
+  )
+  return contract
+}
